56e741d7c473bf9995fe34292b7e101fe119e228
Amin Eid <amin.eid1982@yahoo.com>
2021-01-28 00:23:30 +0200

    First JSX

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo</h1>
+        <p>
+          <h2>Things I need to do:</h2>
+        </p>
+        <ul>
+<li>Learn React</li>
+<li>Climb mtn Everest</li>
+<li>Run A marathon</li>
+<li>Feed the dogs</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
