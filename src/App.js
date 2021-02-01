eae64fcbe9d40d6e64341a536d0a1af62408c2fd
Amin Eid <amin.eid1982@yahoo.com>
2021-02-02 01:06:40 +0200

    lifting -box generator assignment-coding dojo-amin eid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import MessageForm from './components/MessageForm';
+import MessageDisplay from './components/MessageDisplay';
+    
+    
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentMsg, setCurrentMsg] = useState([]);
+    
+    const youveGotMail = ( newMessage ) => {
+        setCurrentMsg(currentMsg.concat(newMessage) );
+    }
+    
+    return (
+        <>
+            <MessageForm onNewMessage={ youveGotMail } />
+            <MessageDisplay message={ currentMsg } />
+        </>
+    );
 }
-
+    
 export default App;
