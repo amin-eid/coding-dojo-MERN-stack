abf3697648f4945eb677e3ad2490a8aa1780e55d
Amin Eid <amin.eid1982@yahoo.com>
2021-02-03 11:47:15 +0200

    Routing Practice assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Home from './components/Home';
+import {Router} from '@reach/router';
 import './App.css';
+import Number from './components/Number';
+import Colors from './components/Colors';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Home path='/home'/>
+        <Number path='/:num' />
+        <Colors path='/:str/:color/:back' />
+      </Router>
+      
     </div>
   );
 }
