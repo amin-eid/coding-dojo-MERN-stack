99ebd7e1c3a94eaa075c6073b78388e94d8fbb2a
Amin Eid <amin.eid1982@yahoo.com>
2021-02-11 12:18:43 +0200

    product manager 3

---
+++ server/routes/product.routes.js
@@ -0,0 +1,9 @@
+const ProductController = require('../controllers/product.controller');
+module.exports = function(app){
+    app.get('/api', ProductController.index);
+    app.post('/api/people/', ProductController.createProduct);
+    app.get('/api/people/', ProductController.getAllProducts);
+    app.get('/api/people/:id', ProductController.getProduct);
+    app.put('/api/people/:id', ProductController.updateProduct);
+    app.delete('/api/people/:id', ProductController.deleteProduct);
+}
